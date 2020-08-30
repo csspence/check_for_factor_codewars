b46c69a1aa781c591dea44f002598e2c50c459c9
Crew Spence <csspence123@gmail.com>
2020-08-30 04:14:16 -0500

    Wrote working solution.

--- check_for_factor_codewars.js
+++ check_for_factor_codewars.js
@@ -15,6 +15,6 @@ For example 2 is not a factor of 7 because: 7 % 2 = 1
 Note: base is a non-negative number, factor is a positive number.
 */ 
 
-function checkForFactor (base, factor) {
-  // code here
+const checkForFactor = (base, factor) => {
+  return base % factor === 0 ? true : false;
 }
